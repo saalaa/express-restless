8f637d19d10b4b74a59f3efa70ec5f0ec320f148
Benoit Myard <bmyard@acute.fr>
2014-04-18 19:12:12 +0200

    update test/pagination to use should

--- test/pagination.js
+++ test/pagination.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('supertest');
+var should = require('should');
 var restless = require('../index');
 
 var pagination = new restless.Resource({
@@ -28,27 +29,61 @@ describe('Pagination handling', function () {
   it('should respond with the default page', function (done) {
     request(server)
       .get('/api/pagination')
-      .expect(new RegExp('100'), done);
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.data.offset.should.equal(0);
+        res.body.data.limit.should.equal(100);
+
+        done();
+      });
   });
 
   it('should respond with the default page for invalid input', function (done) {
     request(server)
       .get('/api/pagination?skip=hello&limit=')
-      .expect(new RegExp('100'), done);
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.data.offset.should.equal(0);
+        res.body.data.limit.should.equal(100);
+
+        done();
+      });
   });
 
   it('should respond with the provided page if valid', function (done) {
     request(server)
       .get('/api/pagination?skip=40&limit=20')
-      .expect(new RegExp('20'))
-      .expect(new RegExp('40'), done);
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.data.offset.should.equal(40);
+        res.body.data.limit.should.equal(20);
+
+        done();
+      });
   });
 
   it('should respond with the adjusted page if invalid', function (done) {
     request(server)
       .get('/api/pagination?skip=40&limit=200')
-      .expect(new RegExp('100'))
-      .expect(new RegExp('40'), done);
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.data.offset.should.equal(40);
+        res.body.data.limit.should.equal(100);
+
+        done();
+      });
   });
 
 });
