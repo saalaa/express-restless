7733dcaf439ba0d344de897cab690f650ed564bb
Benoit Myard <bmyard@acute.fr>
2014-06-06 17:43:30 +0200

    add tests for filters and sorting

---
+++ test/filters.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var request = require('supertest');
+var should = require('should');
+var restless = require('../index');
+
+var filters = new restless.Resource({
+
+  filters: {
+    name: [null, '$in'],
+    age: ['$gt']
+  },
+
+  get_collection: function (req, res, respond) {
+    respond('OK', this.parse(req));
+  }
+
+});
+
+var api = express();
+
+api.use(express.urlencoded());
+api.use(express.json());
+
+restless.install(api);
+
+filters.install(api, '/filters');
+
+var server = express();
+
+server.use('/api', api);
+
+
+// Actual tests
+// ------------
+
+describe('Filters handling', function () {
+
+  it('should respond with the default filters', function (done) {
+    request(server)
+      .get('/api/filters')
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        should(res.body.data.errors).equal(null);
+        should(res.body.data.filters).eql({});
+
+        done();
+      });
+  });
+
+  it('should respond with the filters passed', function (done) {
+    request(server)
+      .get('/api/filters?name=jean&age__$gt=42')
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        should(res.body.data.errors).equal(null);
+
+        should(res.body.data.filters).have.property('name');
+        should(res.body.data.filters).have.property('age');
+
+        done();
+      });
+  });
+
+  it('should respond with a useful error', function (done) {
+    request(server)
+      .get('/api/filters?name=yyy&age=666')
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        should(res.body.data.errors).not.equal(null);
+
+        should(res.body.data.filters).have.property('name', 'yyy');
+
+        should(res.body.data.errors[0]).have.property('error', 'filter');
+        should(res.body.data.errors[0]).have.property('value', 'age');
+
+        done();
+      });
+  });
+
+});
---
+++ test/sorting.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var request = require('supertest');
+var should = require('should');
+var restless = require('../index');
+
+var sorting = new restless.Resource({
+
+  get_collection: function (req, res, respond) {
+    respond('OK', this.parse(req));
+  },
+
+  post_collection: function (req, res, respond) {
+    var inline_options = {
+      sorting: [
+        'name',
+        '-age'
+      ]
+    };
+
+    respond('OK', this.parse(req, inline_options));
+  }
+
+});
+
+var api = express();
+
+api.use(express.urlencoded());
+api.use(express.json());
+
+restless.install(api);
+
+sorting.install(api, '/sorting');
+
+var server = express();
+
+server.use('/api', api);
+
+
+// Actual tests
+// ------------
+
+describe('Sorting handling', function () {
+
+  it('should respond with the default sorting', function (done) {
+    request(server)
+      .get('/api/sorting')
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        should(res.body.data.errors).equal(null);
+        should(res.body.data.sorting).equal(null);
+
+        done();
+      });
+  });
+
+  it('should respond with the sorting passed', function (done) {
+    request(server)
+      .post('/api/sorting?sort=name')
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        should(res.body.data.errors).equal(null);
+
+        should(res.body.data.sorting).have.property('name', 1);
+
+        done();
+      });
+  });
+
+  it('should respond with a useful error', function (done) {
+    request(server)
+      .post('/api/sorting?sort=-age,xxx')
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        should(res.body.data.errors).not.equal(null);
+
+        should(res.body.data.sorting).have.property('age', -1);
+
+        should(res.body.data.errors[0]).have.property('error', 'sorting');
+        should(res.body.data.errors[0]).have.property('value', 'xxx');
+
+        done();
+      });
+  });
+
+});
