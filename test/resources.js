0676ca3a73ebbf38a0396067a2e49459ca821305
Benoit Myard <benoit@saalaa.net>
2014-11-05 17:21:37 +0100

    added sub-resources shortcuts

--- test/resources.js
+++ test/resources.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('supertest');
+var should = require('should');
 var restless = require('../index');
 
+var bbb = new restless.Resource();
+
 var aaa = new restless.Resource({
-  resources: { xxx: true },
+
+  resources: {
+    xxx: true,
+    bbb: bbb
+  },
+
   get_xxx: function (req, res, respond) {
     respond('OK');
   }
+
 });
+
 
 var api = express();
 
@@ -24,12 +34,18 @@ server.use('/api', api);
 // Actual tests
 // ------------
 
-describe('Resources that only define sub-resources', function () {
+describe('Sub-resources', function () {
+  it('Should available as res.resources.sub', function () {
+    should(aaa.resources.bbb).equal(bbb);
+  });
+
+  it('Should available as res.sub', function () {
+    should(aaa.bbb).equal(bbb);
+  });
 
   it('should work as expected', function (done) {
     request(server)
       .get('/api/aaa/111/xxx')
       .expect(200, done);
   });
-
 });
